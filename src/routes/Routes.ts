553cb00a6bec2867ad5f997aa01ef63540926b51
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-05-01 16:46:56 -0300

    feat: adds ProfessionalsROuter to Routes

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
+import ProfessionalsRouter from './ProfessionalsRouter'
 
 const Routes = Router()
 
-Routes.get('/health', (_, res) => res.status(200).send({ success: true }))
+Routes.use('/professionals', ProfessionalsRouter)
 
+Routes.get('/health', (_, res) => res.status(200).send({ success: true }))
 Routes.get('*', (_, res) => res.status(404).send('Not Found'))
 
 export default Routes
