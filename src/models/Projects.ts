171e41057dea6ef16e56161859c1467ae9ca1173
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-05-01 16:27:44 -0300

    feat: create model projects

---
+++ src/models/Projects.ts
@@ -0,0 +1,34 @@
+module.exports = (sequelize: any, DataTypes: any) => {
+  const Projects = sequelize.define(
+    'Projects',
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false
+      },
+      created_at: {
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      schema: 'public',
+      tableName: 'meta_league_projects',
+      timestamps: false
+    }
+  )
+
+  return Projects
+}
