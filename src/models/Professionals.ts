b63d2a9e4cfa85ebf53560b8b764683e4d99c277
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-05-01 16:28:44 -0300

    feat: creates model professionals

---
+++ src/models/Professionals.ts
@@ -0,0 +1,64 @@
+const {Phases, Teams} = require('../models')
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  const Professionals = sequelize.define(
+    'Professionals',
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      birthday_date: {
+        type: DataTypes.STRING(10),
+        allowNull: true
+      },  
+      admission_date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+      },
+      phase_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: Phases,
+          key: 'id'
+        }
+      },
+      team_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: Teams,
+          key: 'id'
+        }
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false
+      },
+      created_at: {
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      schema: 'public',
+      tableName: 'meta_league_professionals',
+      timestamps: false
+    }
+  )
+
+  return Professionals
+}
