84bda686f163e2a0c2c160929f2c7f09e7a6c56d
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-05-01 16:29:13 -0300

    feat: creates model allocations

---
+++ src/models/Allocations.ts
@@ -0,0 +1,56 @@
+const {Professionals, Projects, Managers} = require('../models')
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  const Allocations = sequelize.define(
+    'Allocations',
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        autoIncrement: true
+      },
+      professional_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: Professionals,
+          key: 'id'
+        }
+      },
+      project_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: Projects,
+          key: 'id'
+        }
+      },
+      manager_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: Managers,
+          key: 'id'
+        }
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false
+      },
+      created_at: {
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      schema: 'public',
+      tableName: 'meta_league_allocations',
+      timestamps: false
+    }
+  )
+
+  return Allocations
+}
