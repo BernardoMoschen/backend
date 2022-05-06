da9db8ae6f9c158203019d8a11a81aabb86915ab
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-05-06 14:43:06 -0300

    ref: adds projects router to routes

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import ProfessionalsRouter from './ProfessionalsRouter'
+import ProjectsRouter from './ProjectsRouter'
 
 const Routes = Router()
 
 Routes.use('/professionals', ProfessionalsRouter)
+Routes.use('/projects', ProjectsRouter)
 
 Routes.get('/health', (_, res) => res.status(200).send({ success: true }))
 Routes.get('*', (_, res) => res.status(404).send('Not Found'))
