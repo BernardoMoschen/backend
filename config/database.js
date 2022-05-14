3377a4313dea11e654462d43ad1b6214d130229a
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-05-14 11:07:25 -0300

    ref: databases now uses env variables

--- config/database.js
+++ config/database.js
@@ -2,9 +2,10 @@ require('dotenv').config()
 
 
 module.exports = {
-  username: 'postgres',
-  password: 'MetaLeague',
-  database: 'postgres',
-  host: 'localhost',
-  dialect: 'postgres',
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
 }
