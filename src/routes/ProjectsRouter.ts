53ead9b30ecb76e0b14edfb1e74a84883a95b031
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-05-22 12:27:07 -0300

    feat: creates route to create new project

--- src/routes/ProjectsRouter.ts
+++ src/routes/ProjectsRouter.ts
@@ -5,5 +5,6 @@ const ProjectsRouter = Router()
 
 ProjectsRouter.get('/get-all', ProjectsController.getAllProjects)
 ProjectsRouter.get('/summary-by-id', ProjectsController.getProjectSummaryById)
+ProjectsRouter.post('/create', ProjectsController.createNewProject)
 
 export default ProjectsRouter
